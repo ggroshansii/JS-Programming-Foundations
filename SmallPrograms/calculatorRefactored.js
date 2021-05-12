9a174573640816d01f944bfbe9a926fac8d5934e
ggroshansii <garthgroshansii@gmail.com>
2021-05-12 09:38:01 -0400

    added invalidNumber and invalidOperation functions to check for valid inputs

--- SmallPrograms/calculatorRefactored.js
+++ SmallPrograms/calculatorRefactored.js
@@ -1,7 +1,26 @@
 const readline = require('readline-sync');
 
-function prompt(str){
+function prompt(str) {
   console.log(`=> ${str}`)
+}
+
+function invalidNumber(num) {
+  return Number.isNaN(Number(num)) || num.trim() === ""
+}
+
+function invalidOperation(input) {
+  switch(input) {
+    case "1":
+      return false;
+    case "2":
+      return false;
+    case "3":
+      return false;
+    case "4":
+      return false;
+    default:
+      return true;
+  }
 }
 
 console.log('Welcome to Calculator!');
@@ -9,11 +28,26 @@ console.log('Welcome to Calculator!');
 prompt("What's the first number?");
 let number1 = readline.question();
 
+while (invalidNumber(number1)) {
+  console.log("You have entered an invalid number, please try again");
+  number1 = readline.question()
+}
+
 prompt("What's the second number?");
 let number2 = readline.question();
 
+while (invalidNumber(number2)) {
+  console.log("You have entered an invalid number, please try again");
+  number2 = readline.question();
+}
+
 prompt("Select an operation:\n 1) Add, 2) Subtract, 3) Multiply, 4) Divide");
 let operation = readline.question();
+
+while (invalidOperation(operation)) {
+  console.log("You have entered an invalid operation, please try again")
+  operation = readline.question();
+}
 
 let output;
 switch(operation) {
