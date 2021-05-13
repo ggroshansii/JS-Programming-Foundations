b7648c00c53bd8d0a252f0f40511988a14f2c7a8
ggroshansii <garthgroshansii@gmail.com>
2021-05-13 09:18:21 -0400

    refactored BigRoom.js

--- SmallProblems/BigRoom.js
+++ SmallProblems/BigRoom.js
@@ -1,11 +1,16 @@
 
 let readline = require('readline-sync');
 
+const SQMETERS_TO_SQFEET = 10.7639;
+
 console.log("Enter the length: ");
 let length = readline.question();
 
 console.log("Enter the width: ");
 let width = readline.question();
 
-console.log(`Area of the room in meters: ${Number(length) * Number(width)}`);
-console.log(`Area of the room in feet: ${(((Number(length) * Number(width)) * 10.7639)).toFixed(2)}`);
+let areaInMeters = Number(length) * Number(width);
+let areaInFeet = (((Number(length) * Number(width)) * SQMETERS_TO_SQFEET)).toFixed(2);
+
+console.log(`Area of the room in meters: ${areaInMeters}`);
+console.log(`Area of the room in feet: ${areaInFeet}`);
