53c4cace7c87864f356e68148aedc5c0e6ed5aee
ggroshansii <garthgroshansii@gmail.com>
2021-05-13 08:16:35 -0400

    OddNumbers.js exercise complete

---
+++ SmallProblems/OddNumbers.js
@@ -0,0 +1,12 @@
+for (let index = 0; index <= 99; index++) {
+  if (index % 2 !== 0) {
+    console.log(index);
+  }
+}
+
+
+//ALTERNATIVE SOLUTION..
+
+for (let number = 1; number < 100; number += 2) {
+  console.log(number);
+}
