1a3023b94ce96415b36595e70bd1b05a18249f31
ggroshansii <garthgroshansii@gmail.com>
2021-05-17 21:46:53 -0400

    started monthly loan payment function but not working as of right now

--- SmallPrograms/Assignment-LoanCalculator/LoanCalculator.js
+++ SmallPrograms/Assignment-LoanCalculator/LoanCalculator.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync');
 const json = require("./LoanCalculatorMessages.json");
 
-
+/////////////////////////////////////////////
 function prompt(str) {
     return console.log(`==> ${str}`);
 }
@@ -13,11 +13,14 @@ function message(message) {
 function monthlyInterestRate(APR, loanDuration){
     if (APR.split("").includes('%')){
         APR = APR.split("").splice(0, APR.length-1).join("");
-        console.log(APR);
     }
-    return `Your monlthy interest rate is ${Number(APR) / Number(loanDuration)}%`;
+    return `${Number(APR) / Number(loanDuration)}`;
 }
 
+function monthlyPayment(loanAmount, loanDuration) {
+    let monthly = (Number(loanAmount) * monthlyInterestRate(APR, loanDuration)) / (1 - Math.pow(1 + (monthlyInterestRate(APR, loanDuration)), Number((-loanDuration))));
+    return monthly;
+}
 
 console.log(message("welcome"));
 
@@ -31,3 +34,4 @@ prompt(message("loanDuration"));
 let loanDuration = readline.question();
 
 console.log(monthlyInterestRate(APR, loanDuration));
+console.log(monthlyPayment(loanAmount, loanDuration));
