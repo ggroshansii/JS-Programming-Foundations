d11d9a50688111b4d3244c34cdbc7e8b036ffe3d
ggroshansii <garthgroshansii@gmail.com>
2021-05-20 07:26:23 -0400

    OddLists.js exercise complete

---
+++ SmallProblems/Easy 2/OddLists.js
@@ -0,0 +1,17 @@
+
+function oddities(arr) {
+  let oddIndices = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    if (index === 0 || index % 2 === 0) {
+      oddIndices.push(arr[index]);
+    }
+  }
+  return oddIndices;
+}
+
+console.log(oddities([2, 3, 4, 5, 6])); // logs [2, 4, 6]
+console.log(oddities([1, 2, 3, 4, 5, 6])); // logs [1, 3, 5]
+console.log(oddities(["abc", "def"])); // logs ['abc']
+console.log(oddities([123])); // logs [123]
+console.log(oddities([])); // logs []
