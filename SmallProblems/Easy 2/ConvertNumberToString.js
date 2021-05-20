5a73b3205a67876e12d0e90ac3915876a2a867a2
ggroshansii <garthgroshansii@gmail.com>
2021-05-20 10:13:10 -0400

    ConvertNumberToString.js exercise complete

--- SmallProblems/Easy 2/ConvertNumberToString.js
+++ SmallProblems/Easy 2/ConvertNumberToString.js
@@ -1,16 +1,10 @@
-
 function integerToString(number) {
-
   let numberArray = [];
-  if (number === 0) {
-    numberArray.unshift(number);
-    return numberArray.join("");
-  }
-
-  while (number >= 1) {
+  do {
     let integer = number % 10;
     numberArray.unshift(integer);
     number = Math.floor(number / 10);
-  }
+  } while (number > 0);
+
   return numberArray.join("");
 }
