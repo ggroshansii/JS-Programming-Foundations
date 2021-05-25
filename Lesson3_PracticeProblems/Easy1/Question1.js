8c929e94de6cff66353fb1af62769673595148de
ggroshansii <garthgroshansii@gmail.com>
2021-05-25 11:45:23 -0400

    Easy1 - Question 1 complete

---
+++ Lesson3_PracticeProblems/Easy1/Question1.js
@@ -0,0 +1,17 @@
+
+//Will the code below raise an error?
+
+function foo(a) {
+  return 6;
+}
+
+console.log(foo([1, 2, 3]));
+
+//BONUS:
+
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+numbers[4];  // what will this line return?
+
+console.log(numbers[4]);
+console.log(numbers);
