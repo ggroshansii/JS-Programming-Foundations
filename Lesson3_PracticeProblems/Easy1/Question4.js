b71867362540c2c6e4ab0c643bb9534ecdff58ac
ggroshansii <garthgroshansii@gmail.com>
2021-05-25 13:27:40 -0400

    Easy 1 Question 4 complete

---
+++ Lesson3_PracticeProblems/Easy1/Question4.js
@@ -0,0 +1,16 @@
+
+//Using the following string, create a new string that contains all lowercase letters except for the first character, which should be capitalized. (See the example output.)
+
+let munstersDescription = "the Munsters are CREEPY and Spooky.";
+// => The munsters are creepy and spooky.
+
+
+//my solution
+
+let sentenceCase = munstersDescription.slice(0,1).toUpperCase() + munstersDescription.slice(1).toLowerCase();
+console.log(sentenceCase)
+
+//actual solution
+
+munstersDescription.charAt(0).toUpperCase() +
+  munstersDescription.substring(1).toLowerCase();
