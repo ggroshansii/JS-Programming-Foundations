66e571b9864fe162ce462f2c042a07c885c84cb8
ggroshansii <garthgroshansii@gmail.com>
2021-05-25 13:49:07 -0400

    Easy 1 Question 8 complete

--- Lesson3_PracticeProblems/Easy1/Question8.js
+++ Lesson3_PracticeProblems/Easy1/Question8.js
@@ -7,3 +7,15 @@ let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
 //my solution
 
 flintstones.push("Dino");
+
+//other solutions
+
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+flintstones = flintstones.concat("Dino");
+flintstones; // => [ 'Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles', 'Dino' ]
+
+////
+
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+flintstones[flintstones.length] = "Dino";
+flintstones; // => [ 'Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles', 'Dino' ]
