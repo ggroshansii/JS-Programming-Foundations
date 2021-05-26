7ca06686df6f65c78a3a159c88751594941e03e1
ggroshansii <garthgroshansii@gmail.com>
2021-05-26 07:34:58 -0400

    Easy 2 Question 1 complete

---
+++ Lesson3_PracticeProblems/Easy2/Question1.js
@@ -0,0 +1,11 @@
+
+//Given a string, return a new string that replaces every occurrence of the word "important" with "urgent":
+
+let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+
+//my solution
+
+console.log(advice.replace('important', 'urgent'));
+
+//.replaceAll() replaces all occurrences instead of the first occurrence
