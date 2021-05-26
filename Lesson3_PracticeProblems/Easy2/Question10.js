3aabc9e1ae2d7fda45165770264a8d141b26db1b
ggroshansii <garthgroshansii@gmail.com>
2021-05-26 15:46:54 -0400

    Easy 2 Question 10 complete

---
+++ Lesson3_PracticeProblems/Easy2/Question10.js
@@ -0,0 +1,10 @@
+
+//Write two one-line expressions to count the number of lower-case t characters in each of the following strings:
+
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+
+//my solution / actual solution
+
+console.log(statement1.split("").filter(char => char === 't').length);
