a405ed7b3ef5c8638fce42e4c6ebe88a0b04a645
ggroshansii <garthgroshansii@gmail.com>
2021-05-26 08:19:35 -0400

    Easy 2 Question 4 complete

---
+++ Lesson3_PracticeProblems/Easy2/Question4.js
@@ -0,0 +1,13 @@
+
+//Starting with the string
+
+let famousWords = "seven years ago...";
+
+//show two different ways to put the expected "Four score and " in front of it.
+
+
+//my solution / actual solution
+
+"Four score and " + famousWords;
+
+"Four score and ".concat(famousWords);
