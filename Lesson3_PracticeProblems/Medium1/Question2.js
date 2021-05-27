7d77aad00c56a610757e9e5947eecdc65f55d6f8
ggroshansii <garthgroshansii@gmail.com>
2021-05-27 08:36:31 -0400

    Medium 1 Question 2 complete

---
+++ Lesson3_PracticeProblems/Medium1/Question2.js
@@ -0,0 +1,32 @@
+
+//Starting with the string:
+
+let munstersDescription = "The Munsters are creepy and spooky.";
+
+//Return a new string that swaps the case of all of the letters:
+
+`tHE mUNSTERS ARE CREEPY AND SPOOKY.`
+
+
+//my solution
+
+for (let i = 0; i < munstersDescription; i++) {
+  if (munstersDescription[i] === munstersDescription[i].toLowerCase()) {
+    munstersDescription[i].toUpperCase();
+  } else {
+    munstersDescription[i].toLowerCase();
+  }
+}
+
+console.log(munstersDescription);
+
+
+//actual solution
+
+munstersDescription.split("").map(function(char) {
+  if (char === char.toUpperCase()) {
+    return char.toLowerCase();
+  } else {
+    return char.toUpperCase();
+  }
+}).join("");
